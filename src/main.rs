354e47b8f92ccd72f41da9141e96053a47aed171
NaokiM03 <mattsun.phone@gmail.com>
2020-03-29 02:19:55 +0900

    フォルダ内の全てのファイル名を取得する

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ use rust_embed::RustEmbed;
 struct Asset;
 
 fn main() {
-    let louise = Asset::get("Louise.txt").unwrap();
+    let louise = Asset::get("character/Louise.txt").unwrap();
     let text = std::str::from_utf8(louise.as_ref()).unwrap();
     set_clipboard_string(text).expect("Success");
+
+    for file in Asset::iter() {
+        println!("{}", file.as_ref());
+    }
 
     println!("Hello, world!");
 }
