02e36a218a0d4ac5f3c94a166da7df2a11fff774
NaokiM03 <mattsun.phone@gmail.com>
2020-03-29 02:02:53 +0900

    rust-embed 経由でファイルの内容をクリップボードにセットする

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use clipboard_win::{set_clipboard_string};
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "snippet/"]
+struct Asset;
 
 fn main() {
-    let text = "Hello, world!";
+    let louise = Asset::get("Louise.txt").unwrap();
+    let text = std::str::from_utf8(louise.as_ref()).unwrap();
     set_clipboard_string(text).expect("Success");
 
     println!("Hello, world!");
